28cdccfc4e0cfe27acce1d215d956784967468bd
Loski <maxime.lavaste@gmail.com>
2017-10-19 18:10:08 +0200

    ajout out AC

--- src/fr/upmc/datacenter/software/controller/ports/AdmissionControllerManagementOutboundPort.java
+++ src/fr/upmc/datacenter/software/controller/ports/AdmissionControllerManagementOutboundPort.java
@@ -2,15 +2,21 @@ package fr.upmc.datacenter.software.controller.ports;
 
 import fr.upmc.components.ComponentI;
 import fr.upmc.components.ports.AbstractOutboundPort;
+import fr.upmc.datacenter.software.controller.AdmissionController;
+import fr.upmc.datacenter.software.controller.interfaces.AdmissionControllerManagementI;
 
 public class AdmissionControllerManagementOutboundPort extends		AbstractOutboundPort
 implements	AdmissionControllerManagementI{
 
 	public AdmissionControllerManagementOutboundPort(Class<?> implementedInterface, ComponentI owner) throws Exception {
+		super(AdmissionControllerManagementI.class, owner);
+		assert	owner != null && owner instanceof AdmissionController ;
 	}
 
 	public AdmissionControllerManagementOutboundPort(String uri, Class<?> implementedInterface, ComponentI owner)
 			throws Exception {
+		super(uri, AdmissionControllerManagementI.class, owner);
+		assert	owner != null && owner instanceof AdmissionController ;
 	}
 
 }
