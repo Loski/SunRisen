1fb6b0d61c33c3cca986ba22f99fd31ff33b86c9
Yamihoshi <a4arkane@gmail.com>
2017-11-16 12:56:22 +0100

    Javassist Body Connector

--- src/fr/upmc/javassist/ClassCreator.java
+++ src/fr/upmc/javassist/ClassCreator.java
@@ -1,27 +1,34 @@
 package fr.upmc.javassist;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.List;
 
+import fr.upmc.components.connectors.AbstractConnector;
 import fr.upmc.datacenter.software.connectors.RequestNotificationConnector;
 import fr.upmc.datacenter.software.interfaces.RequestNotificationI;
+import fr.upmc.datacenter.software.requestdispatcher.interfaces.RequestDispatcherManagementI;
 import javassist.CannotCompileException;
 import javassist.ClassMap;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtConstructor;
 import javassist.CtMethod;
+import javassist.CtNewConstructor;
 import javassist.CtNewMethod;
 import javassist.NotFoundException;
+import javassist.bytecode.LocalVariableAttribute;
+import javassist.bytecode.MethodInfo;
 
 public abstract class ClassCreator {
 
 	public static void main (String[] args)
 	{		
 		try {
-
-		RequestNotificationI obj = (RequestNotificationI) createConnectorImplementingInterface("Test",RequestNotificationI.class).newInstance();
+				Constructor ctr = createConnectorImplementingInterface("Test",RequestNotificationI.class).getDeclaredConstructor();
+				AbstractConnector instance = (AbstractConnector) ctr.newInstance();
 			
-		System.out.println(obj);
+				System.out.println(instance);
 			
 			
 		} catch (Exception e) {
@@ -34,15 +41,20 @@ public abstract class ClassCreator {
 	{
 		ClassPool pool = ClassPool.getDefault();
 		
+		CtClass extendsClass = pool.getCtClass(AbstractConnector.class.getName());
+		
 		CtClass test = pool.makeClass("Test");
+		
+		test.setSuperclass(extendsClass);
 		
 		CtClass interfaceToImplement = pool.get(interfaceToImplementClass.getCanonicalName());
 		
 		test.addInterface(interfaceToImplement);
 		
-		CtConstructor constructor = new CtConstructor(new CtClass[0], test);
-		constructor.setBody("return $0;");//Mais que fais $0 ?
-		test.addConstructor(constructor);
+		/*CtConstructor constructor = CtNewConstructor.make(null, null, test);
+		constructor.setBody("{}");
+		System.out.println(constructor);
+		test.addConstructor(constructor);*/
 		
 		
 		for(Method m : interfaceToImplementClass.getDeclaredMethods())
@@ -50,7 +62,8 @@ public abstract class ClassCreator {
 			System.out.println(m);
 			CtMethod method = copyMethodSignature(m,test);
 			
-			method.setBody(createBodyOfConnector());	
+			method.setBody(createBodyOfConnector(interfaceToImplementClass.getCanonicalName(),method));	
+			System.out.println(method);
 		}
 		
 		CtMethod toString = new CtMethod(pool.get("java.lang.String"),"toString",null, test);
@@ -59,7 +72,7 @@ public abstract class ClassCreator {
 		test.addMethod(toString);
 		
 		Class<?> clazz = pool.toClass(test);
-
+		
 		return clazz;
 
 	}
@@ -69,7 +82,11 @@ public abstract class ClassCreator {
 		ClassPool pool = ClassPool.getDefault();
 		
 		 CtMethod method = new CtMethod(pool.get("void"),m.getName(),null, clazz);
-		 method.addParameter(pool.get(m.getParameterTypes()[0].getCanonicalName()));
+		 
+		 for(Class clazzParameter : m.getParameterTypes())
+		 {
+			 method.addParameter(pool.get(clazzParameter.getCanonicalName()));
+		 }	 
 		 
 		 Class[] exceptionsToThrow = m.getExceptionTypes();
 		 
@@ -82,15 +99,27 @@ public abstract class ClassCreator {
 			 i++;
 		 }
 		 
-		 method.setExceptionTypes(exceptions);
-		 
-		 System.out.println(method);
+		 method.setExceptionTypes(exceptions);		 
 		 
 		 return method;
 	}
 	
-	private static String createBodyOfConnector()
+	private static String createBodyOfConnector(String interfaceName,CtMethod method) throws NotFoundException
 	{
-		return "return null;";
+		String s = String.format("( ( %s ) this.offering).%s(",interfaceName,method.getName());
+		
+		 for(int i=1;i<=method.getParameterTypes().length;i++)
+		 {
+			 
+			 s+= "$"+i;
+			 i++;
+			 
+			 if(i<=method.getParameterTypes().length)
+				 s+=", ";
+		 }
+		 
+		 s+=");";
+				 
+		return s;
 	}
 }
